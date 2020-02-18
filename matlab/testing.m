bddd4be44982202adb90ad498f83b063051af206
liang134 <liang134@chapman.edu@bright81.chapman.edu>
2020-02-17 22:22:33 -0800

    new data

---
+++ matlab/testing.m
@@ -0,0 +1,25 @@
+% Run the setup script
+setup
+
+load data/100307_MEG_3-Restin_rmegpreproc.mat
+
+whos
+
+
+cfg = [];
+cfg.hilbert = 'abs';
+
+[hilbert_data] = ft_preprocessing(cfg, data)
+
+plot(hilbert_data.trial{1}(1:1000));
+saveas(gcf, 'output/hilbert_test1.png');
+
+%cfg = [];
+%cfg.method = 'mtmfft';
+%cfg.output = 'pow';
+%cfg.taper = 'hanning';
+%cfg.foi = [12, 30];
+
+%[freq] = ft_freqanalysis(cfg, data)
+
+
