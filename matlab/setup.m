0e580fa2782b929643fd747e49590a5bc7535c1f
andylikescodes <andylikescodes@gmail.com>
2020-03-08 20:21:22 -0700

    new output

--- matlab/setup.m
+++
@@ -1,8 +0,0 @@
-% Set up environment for fieldtrip analysis
-restoredefaultpath
-addpath /home/liang134@chapman.edu/Documents/HCP_MEG/scripts/HCP_MEG_Project/matlab/fieldtrip-20200215
-
-% addpath /home/liang134@chapman.edu/Documents/HCP_MEG/scripts/fieldtrip-r10442
-ft_defaults
-
-
